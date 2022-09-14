7ffda072e36d2b5c4be23c1b13a1603594023e66
Nivi Jammi <nivedita.jammi@gmail.com>
2022-09-14 01:42:48 -0400

    Initial commit

--- src/main/java/com/kenzie/library/Traveler.java
+++ src/main/java/com/kenzie/library/Traveler.java
@@ -1,20 +1,131 @@
 package com.kenzie.library;
 
 public class Traveler {
-//TODO: Code Traveler class
-  
+
+    protected String name;
+    protected int food;
+    protected boolean isHealthy;
+
+    public Traveler(){
+        this.name = "";
+        this.food = 1;
+        this.isHealthy = true;
+
+    }
+
+    public Traveler(String name){
+        this.name = name;
+        this.food = 1;
+        this.isHealthy = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public boolean getIsHealthy() {
+        return isHealthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+        isHealthy = healthy;
+    }
+
+    public void hunt(){
+        setFood(getFood() + 2);
+    }
+
+    public void eat(){
+        if(getFood() >= 1){
+            setFood(food - 1);
+        }else{
+            setHealthy(false);
+        }
+
+    }
 }
 
 class Doctor extends Traveler {
-//TODO: Code Doctor class
-  
-  
+
+    public Doctor() {
+        this.name = "";
+        this.food = 1;
+        this.isHealthy = true;
+    }
+
+    public Doctor(String name) {
+        super(name);
+        this.food = 1;
+        this.isHealthy = true;
+    }
+
+    @Override
+    public void hunt() {
+        super.hunt();
+    }
+
+    @Override
+    public void eat() {
+        super.eat();
+    }
+
+    public void heal(Traveler traveler){
+        traveler.setHealthy(true);
+    }
 }
 
 class Hunter extends Traveler {
-//TODO: Code Hunter class
+    public Hunter() {
+        this.name = "";
+        this.food = 2;
+        this.isHealthy = true;
+    }
 
-  
-  
+    public Hunter(String name) {
+        super(name);
+        this.food = 2;
+        this.isHealthy = true;
+
+    }
+
+    @Override
+    public void hunt() {
+        setFood(getFood() + 5);
+    }
+
+    @Override
+    public void eat() {
+        if(getFood() == 2){
+           setFood(food - 2);
+        }else if(getFood() == 1) {
+            setFood(food - 1);
+            setHealthy(false);
+        }else{
+            setHealthy(false);
+        }
+    }
+    public void giveFood(Traveler traveler, int numOfFoodUnits){
+        if(getFood() >= numOfFoodUnits){
+            int travelersCurrentFoodUnit = traveler.getFood();
+            traveler.setFood(travelersCurrentFoodUnit + numOfFoodUnits);
+            setFood(food - numOfFoodUnits);
+        }else{
+            //No food is transferred - numOfFoodUnits < huntersFoodStock
+            System.out.println("No food is transferred - numOfFoodUnits < huntersFoodStock");
+        }
+
+    }
 }
 
